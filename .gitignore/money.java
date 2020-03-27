ee0c89a6aab718984eb86757456f4e9ec26ebddf
Odium-Mundi <61995344+Odium-Mundi@users.noreply.github.com>
2020-03-27 14:45:23 +0300

    Delete money.java

--- .gitignore/money.java
+++
@@ -1,8 +0,0 @@
-public class money {
-    public static void main(String[]args) {
-        int current_balance = 2_000_000_000;
-        int transfer_amount = 500_000_000;
-        int total = current_balance + transfer_amount;
-            System.out.println("Итого: " + total);
-    }
-}
